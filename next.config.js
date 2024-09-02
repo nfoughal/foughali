5bc5ba1ae3f7a2838388b1b7bde3185ec5250747
nfoughal <nabilfoughali97@gmail.com>
2024-09-02 15:13:06 +0100

    portfolio updeate

--- next.config.js
+++ next.config.js
@@ -10,9 +10,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  basePath: '/foughali', // Replace with your GitHub repository name
-  assetPrefix: '/foughali/', // Replace with your GitHub repository name
-  trailingSlash: true, // Ensures URLs end with a trailing slash
+
 };
 
 module.exports = nextConfig;
